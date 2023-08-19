43605f832da2a77cf6402df638e24e6318bf0861
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-19 09:42:57 +0530

    - dump database connection in fastapi app.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-
+import models, schemas
 from schemas import Blog
+from database import engine
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
 
 @app.post("/")
 def index(request: Blog):
---
+++ models.py
@@ -0,0 +1,9 @@
+from database import Base
+from sqlalchemy import Column, String, Integer
+
+
+class Blog(Base):
+    __tablename__ = "blogs"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    body = Column(String)
