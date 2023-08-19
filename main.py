84a8fd86df40b171955d31879a2e2af7e25eead3
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-19 10:57:10 +0530

    - dump delete operation.
    - All CRUD operations are done here..

--- main.py
+++ main.py
@@ -26,6 +26,19 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with id {id} is not found in db",
+        )
+    blog.update(request.model_dump())  # dict() is deprecated, so use model_dump()
+    db.commit()
+    return "Updated"
+
+
 @app.get("/blogs", status_code=status.HTTP_200_OK)
 def getAll(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
@@ -47,12 +60,12 @@ def getOne(id, response: Response, db: Session = Depends(get_db)):
 
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def deleteOne(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Blog with id {id} is not found in db",
         )
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog.delete(synchronize_session=False)
     db.commit()
     return {"detail": "Done!!"}
