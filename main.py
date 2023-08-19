3776b5bc0745b79e7790d662e6f620fe36177e4d
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-19 10:10:37 +0530

    - dump create, getall and getone blog operations.

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models, schemas
-from schemas import Blog
-from database import engine
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-@app.post("/")
-def index(request: Blog):
-    return request
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/blog")
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get("/blogs")
+def getAll(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{id}")
+def getOne(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
