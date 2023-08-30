54d06d1c28c17ef095fff5b2388d6f0578fe307e
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-30 13:46:47 +0530

    - requires fix.

--- fastapi_projects/mysql_connection/main.py
+++ fastapi_projects/mysql_connection/main.py
@@ -1,3 +1,5 @@
 from fastapi import FastAPI
+# from config.db import Base, SessionLocale, engine
+# from models.model import Users
 
 app = FastAPI()
---
+++ fastapi_projects/mysql_connection/models/model.py
@@ -0,0 +1,9 @@
+from config.db import Base
+from sqlalchemy import Integer, Column, String
+
+class Users(Base):
+    __tablename__ = 'fastapi_users'
+    id=Column(Integer, primary_key=True, index=True)
+    name=Column(String(50))
+    email=Column(String(100), unique=True)
+    password=Column(String(100))
